94832bdd2f442eccdef7baaee952d79588674d22
Igor Fernandes <igor.fernandes@accenture.com>
2019-07-08 23:46:51 -0300

    Show menu

--- app/src/main/java/br/com/igorfs/lodjinha/view/MainActivity.kt
+++ app/src/main/java/br/com/igorfs/lodjinha/view/MainActivity.kt
@@ -1,13 +1,33 @@
 package br.com.igorfs.lodjinha.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import br.com.igorfs.lodjinha.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration : AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.title = getString(R.string.app_name)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+        val drawerLayout : DrawerLayout? = findViewById(R.id.main_drawer_layout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
 }
